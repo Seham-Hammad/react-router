2aa0714179ecfbc72837f85b9a35962c78638067
Seham1981 <seham.hammad@yahoo.ie>
2019-08-30 22:25:28 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './components/Home';
+import Contact from './components/Conatact';
+import About from './components/About';
+import Error from './components/Error';
+import Nav from './components/Nav';
+import UserForm from './components/UserForm';
+import "./App.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state= {
+    repos: null
+  }
+
+  getUser = (e) => {
+  
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if(user){
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data.public_repos;
+        this.setState({
+          repos: repos
+        })
+        console.log(res);
+        console.log(repos)
+      })
+    } else return;   
+  }
+  render(){
+    return (  
+    
+     <BrowserRouter>
+       <div className="App">
+         <header className="App-header">
+           <h1>React Router and Axios Tutorial...</h1>
+         </header>
+             <Nav />
+             <UserForm getUser={this.getUser} />
+              {this.state.repos ? <p>Number of repos: {this.state.repos}</p> :
+              <p>Please enter username!</p>}
+             <Switch>
+                <Route path='/'          component={Home} exact />
+                <Route path='/about'     component={About} />
+                <Route path='/contact'   component={Contact} />
+                
+                <Route component={Error} />
+            </Switch>
+           
+        </div>
+       
+     </BrowserRouter>
+    );
+  }
+  
 }
 
 export default App;
+
+/**this code is equal to the use of if{} but we can not use plain javascript code inside jsx, we use 
+ * ternary operator
+ * {this.state.repos ? <p>Number of repos: {this.state.repos}</p> :
+              <p>Please enter username!</p>}
+
+
+if (this.state.repos){
+  <p>Number of repos: {this.state.repos}</p>
+} else{
+  <p>Please enter a username!</p>
+}
+*/
+
+
+
